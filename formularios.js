a34c241033c07ca78b7f4d75d99b35704f13e874
Sofía Sidoli <sofiasidoli98@gmail.com>
2019-06-07 20:31:19 -0300

    agrego archivos hasta el break

--- formularios.js
+++ formularios.js
@@ -1,7 +1,7 @@
 window.addEventListener("load",main)
 
 function main(){
-// Agarrarle el formulario
+// Agarrarle el formulario. Nunca se agarra el boton. Siempre etiqueta form
 let form = document.querySelector("form")
 let input = document.querySelector("input")
 
@@ -16,7 +16,10 @@ form.addEventListener("submit",handleSubmit)
 
 function handleSubmit(e){
     e.preventDefault()
-    console.log(e.target[0].value)
+    //Cualquier cosa que pueda escribir va a ser el value del input
+    let nombre = e.target[0].value
+    nombre = nombre.trim()
+    console.log(nombre.length)
 }
 }
 
@@ -24,4 +27,16 @@ function handleSubmit(e){
 
 /*
 submit: es un evento que se dispara cuando un fomulario intenta ser enviado
+
+String.length => Number - La longitud del string
+
+String.trim() => String - Devuelve el mismo string pero sin espacios 
+antes ni despues
+
+{String|Array}.indexOf() => Number - Retorna la posicion de la 
+instancia del elemento X ó -1 si no se encuentra
+
+{String|Array}.includes() => Boolean. Retorna verdadero o falso
+dependiendo si X se encuantra o no
+
 */
