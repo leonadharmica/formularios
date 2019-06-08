a57dea98dcf591bdd40d21022f111a01d6993431
Sofía Sidoli <sofiasidoli98@gmail.com>
2019-06-07 21:59:51 -0300

    subo lo de la segunda clase de formularios con ejercicio a realizar

--- formularios.js
+++ formularios.js
@@ -3,7 +3,50 @@ window.addEventListener("load",main)
 function main(){
 // Agarrarle el formulario. Nunca se agarra el boton. Siempre etiqueta form
 let form = document.querySelector("form")
-let input = document.querySelector("input")
+let nombre = document.querySelector("#nombre")
+
+//change : Se dispara cuando el control de formulario cambia de valor
+
+let paises = {
+    ar : ["Buenos Aires", "La Pampa"],
+    ve : ["Margarita", "Caracas"],
+    ch : ["Beijing", "Shangai"]
+}
+
+/*
+1) Seleccionar el select
+2) Asignarle un evento change
+3) El callback del change tiene que poder agarrar el valor del select
+4) Con el valor obtenido, buscar en el objeto "paises" la propiedad 
+que le corresponda
+5) Si la propiedad no existe se tiene que ver en pantalla un mensaje 
+que diga "Tiene que seleccionar una opcion" en color rojo
+6) Si la propiedad existe, tenemos que recorrer el array
+que tiene como valor asociado
+7) Por cada valor del array se tiene que crear un <option>
+8) Agregar todos los <option> creados a un nuevo <select>
+9) Agregar el <select> al DOM
+
+*/
+
+let select = document.querySelector("select")
+
+select.addEventListener("change", mostrarProv)
+
+function mostrarProv(){
+    let valor = select.value
+    // paises.ve = paises[pais]
+    let provincias = paises[valor]
+
+    if (provincias == undefined) {
+        let parrafo = document.createElement('p')
+        let texto = document.createTextNode('Tiene que seleccionar una opcion')
+        parrafo.appendChild(texto)
+        document.body.appendChild(parrafo);
+    }
+
+}
+
 
 //Asignar un submit
 
@@ -38,5 +81,7 @@ instancia del elemento X ó -1 si no se encuentra
 
 {String|Array}.includes() => Boolean. Retorna verdadero o falso
 dependiendo si X se encuantra o no
+
+MAYUSCULAS Y MINUSCULAS
 
 */
